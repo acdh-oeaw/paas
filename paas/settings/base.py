b41864f799d062911f9c5ef895e89a34a3fdd18b
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-12-10 20:39:14 +0100

    links to visualisations added

--- paas/settings/base.py
+++ paas/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'apis_core.apis_relations',
     'apis_core.apis_vocabularies',
     'apis_core.apis_labels',
+    'apis_core.apis_vis',
     'rest_framework.authtoken',
     'guardian',
     'charts',
