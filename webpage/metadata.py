f71c05680673a856af76f48acec41f53b4d89ff3
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-01-15 10:40:12 +0100

    updated paratexts, logo and image fixed #38

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -4,10 +4,9 @@
 PROJECT_METADATA = {
     'title': 'MPR',
     'author': 'Matthias Schlögl, Peter Andorfer',
-    'subtitle': 'Entities of the "Ministerratsprotokolle"',
-    'description': 'Entity store of the digital edition of the \
-    "Ministerratsprotokolle"',
-    'github': 'https://github.com/acdh-oeaw/apis-core',
+    'subtitle': 'Entities mentioned in the "Minutes of the council of the ministers of Austria and of the Austro-Hungarian Monarch"',
+    'description': """The edition 'Minutes of the Austrian Council of Ministers’ was launched in 1967 by Friedrich Engel-Janosi in close cooperation with Hungarian historians. It is a full-text and annotated edition of the original documents kept in the Austrian State Archives (Haus-, Hof- und Staatsarchiv and Allgemeines Verwaltungsarchiv) (see fig. 1). The minutes record the sessions of the Council of Ministers founded in 1848 (1852−1861 the Conference of Ministers) of the Austrian Empire until the Austro-Hungarian Compromise of 1867 (1. Serie). The Compromise transformed the Austrian Empire into the Austro-Hungarian Monarchy. Three bodies emerged from the until then unified Council of Ministers: the joint Council of Ministers of the Austro-Hungarian Monarchy 1867–1918 (2. Serie), a Council of Ministers for the Kingdom of Hungary and one for the remaining countries, abbreviated as Austria or Cisleithania. The minutes of the latter's sessions will be edited as the minutes of the Cisleithanian Council of Ministers, 1867−1918 (3. Serie).The Council of Ministers was the central body of government. The minutes of its sessions reflect all aspects of political life, from issues concerning the state’s structure and organisation to social, economic and technical developments as well as cultural and social problems. The minutes represent an extraordinary historical source. The current web application considers itself as an 'Entity-Hub' of all Entities (Persons, Places, Institutions, Works and Evenets) mentioned in the Minutes.""",
+    'github': 'https://github.com/acdh-oeaw/paas/tree/mpr_webpage_package',
     'purpose_de': '',
     'purpose_en': '',
     'version': '0.0.1',
