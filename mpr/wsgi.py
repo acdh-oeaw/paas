821909b52b19a86bef8ec3ca63c7387fe0a6e5d1
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2019-01-22 17:53:47 +0100

    whater changes where done on the server....

--- mpr/wsgi.py
+++ mpr/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+sys.path.append('/var/www/html/mprdev')
+sys.path.append('/var/www/html/mprdev/myenv/lib/python3.6/site-packages')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mpr.settings.server")
